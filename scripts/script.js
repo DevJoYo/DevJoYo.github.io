3309c5943059e939e456ad622de5bb2785738343
SmlRat <exaltedJJ@gmail.com>
2025-03-06 20:11:10 -0500

    completed nav bar links

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,24 @@
-/*let item = document.getElementById("main");
-
-  window.addEventListener("wheel", function (e) {
-    if (e.deltaY > 0) item.scrollLeft += 100;
-    else item.scrollLeft -= 100;
-  });
-
-  */
-
-  /*
-  window.addEventListener('wheel', function(event) {
-    if (event.deltaY > 0) {
-        // Scroll down, move right
-        window.scrollBy(100, 0); // Scroll 100px to the right
-    } else {
-        // Scroll up, move left
-        window.scrollBy(-100, 0); // Scroll 100px to the left
-    }
-}, { passive: true });
-*/
-//let screenWidth = window.innerWidth;
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    let item = document.getElementById("main");
+    const view = document.getElementById("main");
 
-    if (item) {
+    if (view) {
         window.addEventListener('wheel', function(event) {
             if (event.deltaY > 0) {
                 // Scroll down, move window right
-                item.scrollLeft += 100; 
+                view.scrollLeft += 100; 
             } else {
                 // Scroll up, move window left
-                item.scrollLeft -= 100;
+                view.scrollLeft -= 100;
+            }
+        }, { passive: true });
+        window.addEventListener("keydown", function(event) {
+            if(event.key === "ArrowLeft")
+            {
+                view.scrollLeft -= 100; 
+            }
+            else if(event.key === "ArrowRight")
+            {
+                view.scrollLeft += 100;
             }
         }, { passive: true });
     } else {
